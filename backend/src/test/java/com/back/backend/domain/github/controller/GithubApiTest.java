f551a34785ee4f3c0b539b794d2bfc542172f5c8
gurum505 <sayhojeong505@gmail.com>
2026-03-27 11:21:40 +0900

    fix : wireMock의 참조 classPath 수정

--- backend/src/test/java/com/back/backend/domain/github/controller/GithubApiTest.java
+++ backend/src/test/java/com/back/backend/domain/github/controller/GithubApiTest.java
@@ -27,6 +27,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
