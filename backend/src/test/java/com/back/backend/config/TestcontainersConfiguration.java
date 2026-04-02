85e2f2ad5884fd1a1b1db2362d4aff8b5be31263
Jiny <lozy9604@naver.com>
2026-04-02 15:29:21 +0900

    fix: TestcontainersConfiguration 롤백(DynamicPropertyRegistrar 사용 안하도록 변경) TestContainer 내부에서 Redis를 설정하는 게 아니라 그냥 test.yml 워크플로우에 services.redis 추가

--- backend/src/test/java/com/back/backend/config/TestcontainersConfiguration.java
+++ backend/src/test/java/com/back/backend/config/TestcontainersConfiguration.java
@@ -3,7 +3,6 @@ package com.back.backend.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -22,18 +21,11 @@ public class TestcontainersConfiguration {
     }
 
     @Bean
+    @ServiceConnection(name = "redis")
     GenericContainer<?> redisContainer() {
         return new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
             .withTmpFs(Map.of("/data", "rw"))
             .withExposedPorts(6379)
             .withReuse(true);
-    }
-
-    @Bean
-    DynamicPropertyRegistrar redisProperties(GenericContainer<?> redisContainer) {
-        return registry -> {
-            registry.add("spring.data.redis.host", redisContainer::getHost);
-            registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379));
-        };
     }
 }
